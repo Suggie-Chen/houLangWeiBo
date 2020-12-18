fd6aadade495981fcfe49616aee3e84447fc6516
Jennifer231 <737662744@qq.com>
2020-12-18 12:10:57 +0800

    merge

--- src/main/java/cn/edu/bupt/ch11_4/controller/UserController.java
+++ src/main/java/cn/edu/bupt/ch11_4/controller/UserController.java
@@ -41,10 +41,11 @@ public class UserController {
         return "user/home";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/login/**")
     String login(Model model){
-        return "/login";
+        return "login";
     }
+
 
     @GetMapping("/register")
     String register(){
@@ -56,6 +57,7 @@ public class UserController {
         SysUser sysUser = sysUserRepository.findByUsername(username);
         if(sysUser != null){
             model.addAttribute("msg","用户名已存在！");
+            System.out.println("注册失败处理=============用户名已存在");
             return "user/register";
         }else {
             PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
@@ -63,14 +65,15 @@ public class UserController {
             sysUser.setUsername(username);
             sysUser.setPassword(passwordEncoder.encode(password));
 //            if(username.equals("admin")){
-                SysRole sysRole = new SysRole();
-                sysRole.setType("ROLE_USER");
-                sysRoleRepository.save(sysRole);
-                List<SysRole> roles = new ArrayList<SysRole>();
-                roles.add(sysRole);
-                sysUser.setRoles(roles);
+            SysRole sysRole = new SysRole();
+            sysRole.setType("ROLE_USER");
+            sysRoleRepository.save(sysRole);
+            List<SysRole> roles = new ArrayList<SysRole>();
+            roles.add(sysRole);
+            sysUser.setRoles(roles);
 //            }
             sysUserRepository.save(sysUser);
+            System.out.println("注册成功处理=============");
             return "redirect:/user/login";
 
         }
