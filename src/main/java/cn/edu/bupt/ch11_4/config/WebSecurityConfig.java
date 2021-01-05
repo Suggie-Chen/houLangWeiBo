a41c6ca02950fe639e2889dbef469cf2fba6c362
姝仪 陈 <929946014@qq.com>
2021-01-05 21:44:15 +0800

    实现了游客模式

--- src/main/java/cn/edu/bupt/ch11_4/config/WebSecurityConfig.java
+++ src/main/java/cn/edu/bupt/ch11_4/config/WebSecurityConfig.java
@@ -31,54 +31,57 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .frameOptions().disable()
                 //针对h2-console关闭csrf验证
                 .and().csrf()
-                    .ignoringAntMatchers("/h2-console/**","/user/**","/tourist/**")
+                .ignoringAntMatchers("/h2-console/**","/user/**","/tourist/**")
                 .and().formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
-                        System.out.println("登陆成功处理==============");
-                            //跳转到微博整体首页 ，配置登录成功的回调
-                            httpServletResponse.sendRedirect("/user/home");
-                        })
-                    .failureForwardUrl("/loginfailure?error=true")
-                    .failureHandler((httpServletRequest, httpServletResponse, e) -> {
-                            System.out.println("登陆失败处理=============重定向到登录页面");
-                            //返回到登陆页面，可以采用AJAX技术实现，提示用户用户名不存在，密码不正确之类的错误
-                            System.out.println(e.getMessage());
-                            String msg = null;
-                            if (e instanceof LockedException) {
-                                msg="账户被锁定，请联系管理员!";
-                            } else if (e instanceof CredentialsExpiredException) {
-                                msg="密码过期，请联系管理员!";
-                            } else if (e instanceof AccountExpiredException) {
-                                msg="账户过期，请联系管理员!";
-                            } else if (e instanceof DisabledException) {
-                                msg="账户被禁用，请联系管理员!";
-                            } else if (e instanceof BadCredentialsException) {
-                                msg="用户名或者密码输入错误，请重新输入!";
-                            }
-                            System.out.println(msg);
+                .loginPage("/login")
+                .permitAll()
+                .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    System.out.println("登陆成功处理==============");
+                    //跳转到微博整体首页 ，配置登录成功的回调
+                    httpServletResponse.sendRedirect("/user/home");
+                })
+                .failureForwardUrl("/loginfailure?error=true")
+                .failureHandler((httpServletRequest, httpServletResponse, e) -> {
+                    System.out.println("登陆失败处理=============重定向到登录页面");
+                    //返回到登陆页面，可以采用AJAX技术实现，提示用户用户名不存在，密码不正确之类错误
+                    System.out.println(e.getMessage());
+                    String msg = null;
+                    if (e instanceof LockedException) {
+                        msg="账户被锁定，请联系管理员!";
+                    } else if (e instanceof CredentialsExpiredException) {
+                        msg="密码过期，请联系管理员!";
+                    } else if (e instanceof AccountExpiredException) {
+                        msg="账户过期，请联系管理员!";
+                    } else if (e instanceof DisabledException) {
+                        msg="账户被禁用，请联系管理员!";
+                    } else if (e instanceof BadCredentialsException) {
+                        msg="用户名或者密码输入错误，请重新输入!";
+                    }
+                    System.out.println(msg);
 
-                           httpServletResponse.sendRedirect("/login?error=true");
-                        })
+                    httpServletResponse.sendRedirect("/login?error=true");
+                })
 //                .failureHandler(authenticationFailHandler)
                 .and().logout()
-                    .logoutUrl("/user/logout")
-                    .logoutSuccessHandler(((httpServletRequest, httpServletResponse, authentication) -> {
-                        System.out.println("登出成功处理=============重定向到登录页面");
-                        httpServletResponse.sendRedirect("/user/login");
-                    }))
+                .logoutUrl("/user/logout")
+                .logoutSuccessHandler(((httpServletRequest, httpServletResponse, authentication) -> {
+                    System.out.println("登出成功处理=============重定向到登录页面");
+                    httpServletResponse.sendRedirect("/user/login");
+                }))
                 .and().authorizeRequests()
-                    .antMatchers("/h2-console/**","/user/register","/tourist/**").permitAll()
+//                    .antMatchers("/h2-console/**","/user/register","/tourist/**").permitAll();
+                .antMatchers("/h2-console/**","/user/register","/tourist/**").permitAll();
+
 //                ！！！！！！！！！！！！！！权限管理
-                    .antMatchers("/user/**").hasRole("USER");//Spring Security会自动为任何角色添加前缀ROLE_,使用hasRole表达式来检查当前经过身份验证的主体是否具有指定的权限。
+//                    .antMatchers("/user/**").hasRole("USER");//Spring Security会自动为任何角色添加前缀ROLE_,使用hasRole表达式来检查当前经过身份验证的主体是否具有指定的权限。
 //                    .anyRequest().authenticated();//所有进入应用HTTP请求都要进行认证
+//                .antMatchers("/user/**").authenticated();
 //        ;and 方法的返回值是一个 SecurityBuilder 的子类，其实就是 HttpSecurity，也就是 and 方法总是让我们回到 HttpSecurity，从而开启新一轮的 xxxConfigurer 配置。
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-       auth.userDetailsService(customUserService).passwordEncoder(passwordEncoder);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        auth.userDetailsService(customUserService).passwordEncoder(passwordEncoder);
     }
 }
--- src/main/java/cn/edu/bupt/ch11_4/controller/TouristController.java
+++ src/main/java/cn/edu/bupt/ch11_4/controller/TouristController.java
@@ -1,15 +1,59 @@
 package cn.edu.bupt.ch11_4.controller;
 
 
+import cn.edu.bupt.ch11_4.dao.*;
+import cn.edu.bupt.ch11_4.entity.Message;
+import cn.edu.bupt.ch11_4.entity.SysUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/tourist")
 public class TouristController {
-    @GetMapping("/")
-    String tour(){
+    private MessageRepository messageRepository;
+    private CommentRepository commentRepository;
+    private FanRepository fanRepository;
+
+    @Autowired(required = false)
+    public void setMessageRepository(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+    @Autowired(required = false)
+    public void setCommentRepository(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Autowired
+    SysUserRepository sysUserRepository;
+    @Autowired
+    SysRoleRepository sysRoleRepository;
+
+
+    @GetMapping({"","/home"})
+    String tour(Model model,
+                @RequestParam(value = "start",defaultValue = "0") Integer start,
+                @RequestParam(value = "limit",defaultValue = "9") Integer limit,
+                @RequestParam(value = "sort_method",defaultValue = "time") String sort_method)
+    {
+        //得到当前用户名
+//        SysUser uid = (SysUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        String uname = uid.getUsername();
+
+        start = start <0 ? 0 :start;
+        Sort sort = Sort.by(Sort.Direction.DESC,sort_method);
+        Pageable pageable = PageRequest.of(start,limit,sort);
+        Page<Message> messages = messageRepository.findAll(pageable);
+        model.addAttribute("messages",messages);
+//        model.addAttribute("uname", uname);
         return "tourist/tindex";
     }
 }
